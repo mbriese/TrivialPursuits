37e2bd60e3307aa2950f45a7df5ae7a401ced179
MBriese <mindi.briese@gmail.com>
2024-10-06 20:42:04 -0700

    fixed play or quit button

--- TrivialPursuitGame/src/App.tsx
+++ TrivialPursuitGame/src/App.tsx
@@ -5,8 +5,17 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [playAnswer, setPlayAnswer] = useState(false);
 
+  const [playAnswer, setPlayAnswer] = useState(false);
+  const playHandler = () => {
+    setPlayAnswer(true);
+    setQuitAnswer(false);
+  }
+  const [quitAnswer, setQuitAnswer] = useState(false);
+  const quitHandler = () => {
+    setPlayAnswer(false);
+    setQuitAnswer(true);
+  }
   let [playerName, setName] = useState("");
   let categories = [
     "Arts & Leisure",
@@ -37,79 +46,71 @@ function App() {
         <div className="h2 header">
           <Message/>
         </div>
-        <button
+        { !playAnswer &&
+            <button
             className="h2 container button"
             onClick={(playHandler)}>
           Click to Play Game!
+        </button>}
+        { playAnswer &&
+            <button
+            className="h2 container button"
+            onClick={(quitHandler)}>
+          Click to Quit Game!
         </button>
+        }
 
         { playAnswer &&
-            <div className="h2 container"> "Welcome" </div> }
+            <>
+              <div className="h2 container">{gameName}</div>
+              <div className="topnav container">
+                      <span className="topnav button left">
+                          Back
+                      </span>
+                <span className="topnav button right">
+                          Next
+                      </span>
+              </div>
+              <div className="flex-container child">
+                Enter Player Name
+                <input
+                    type="string"
+                    id="playerName"
+                    name="playerName"
+                    onChange={handleChange}
+                    value={playerName}
+                />
+                {<p>Lets play Trivial Pursuit {playerName}</p>}
+              </div>
+              <div className="h2 container page-section-inner">
+                <p>Categories</p>
+                <p>Select a Category</p>
+                <ul className="page-section-item">
+                  {categories.map((categories) => (
+                      <li key={categories}>{categories}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <ListGroup/>
+                <ListGroup/>
+                <ListGroup/>
+              </div>
+              <div className="h2 flex-container button-item">Previous Category</div>
+              <div className="h2 flex-container button-item">Next Category</div>
+              <button
+                  className="h2 button-item"
+                  onClick={() => setCount((count) => count + 1)}>
+                category score is {count}
+              </button>
+              <button
+                  className="h2 button-item"
+                  onClick={() => setCount((count) => count + 1)}>
+                overall score is {count}
+              </button>
+            </>
+        }
 
-
-        <div className="topnav container">
-              <span className="topnav button left">
-                Back
-              </span>
-          <span className="topnav button right">
-                Next
-              </span>
-        </div>
-
-        <div className="h2 container">{gameName} && {playAnswer}</div>
-        <div className="flex-container child">
-          Enter Player Name
-          <input
-              type="string"
-              id="playerName"
-              name="playerName"
-              onChange={handleChange}
-              value={playerName}
-          />
-          {<p>Lets play Trivial Pursuit {playerName}</p>}
-        </div>
-        <div>
-          <ListGroup/>
-          <ListGroup/>
-          <ListGroup/>
-        </div>
-        <div className="h2 container page-section-inner">
-          <p>Categories</p>
-          <p>Select a Category</p>
-          <ul className="page-section-item">
-            {categories.map((categories) => (
-                <li key={categories}>{categories}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="page-section-item message">
-          this will be category display
-        </div>
-        <div className="h2 page-section-inner-bold message">
-          this will be question display
-        </div>
-        <div className="h2 page-section-inner message">
-          this will be answer1 button
-        </div>
-        <div className="h2 page-section-inner message">
-          this will be answer2 button
-        </div>
-        <div className="h2 page-section-inner message">
-          this will be answer3 button
-        </div>
-
-        <div className="h2 flex-container button-item">Previous Category</div>
-        <div className="h2 flex-container button-item">Next Category</div>
-        <button
-            className="h2 button-item"
-            onClick={() => setCount((count) => count + 1)}>
-          category score is {count}
-        </button>
-        <button
-            className="h2 button-item"
-            onClick={() => setCount((count) => count + 1)}>
-          overall score is {count}
-        </button>
 
       </div>
     </div>
